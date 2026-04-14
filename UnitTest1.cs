e0dbdf615cb127fe07765e9473064ec44c7d065b
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-04-14 09:56:58 +0200

    Simple Ui Initialization Test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,11 +1,27 @@
-﻿namespace MusicTestXUnit
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 
-        }
+public class UiTests
+{
+    [Fact]
+    public void Test_Data_Loads_In_Table()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://localhost:5500");
+
+        // Wait until table rows appear (max 10 sec)
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        wait.Until(d => d.FindElements(By.TagName("tr")).Count > 0);
+
+        var rows = driver.FindElements(By.TagName("tr"));
+
+        Assert.True(rows.Count > 0);
+
+        driver.Quit();
     }
 }
